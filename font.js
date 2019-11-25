5d6093dfdfcdfb2779e26675ff8aa1d9925c928c
joelfooxj <joelfooxj@gmail.com>
2019-11-25 16:31:41 -0500

    Tier1 files added

---
+++ font.js
@@ -0,0 +1,8 @@
+// Features to add: 
+// 1. Shrink nav buttons when screen shrink 
+// 2. Card filter with font search Bar
+// 3. Replace text with custom text 
+// 4. font-size of card text
+// 5. black/white switch 
+// 6. list display vs grid display 
+// 7. Reset to original state 
